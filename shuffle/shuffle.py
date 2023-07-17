514d316821dba2e017f4799ff25774957c7c9d48
Amit Kumar <akumar105@sapient.com>
2023-07-17 15:02:25 +0100

    Fix pylint issues (#7)
    * Update shuffle.py
    
    * fix pylint errors
    
    R0205, C0116, R0903
    
    * syntax error
    
    * fix pylint
    
    C0114
    C0115
    C0116
    C0103
    
    * fix R1716
    
    ---------
    
    Co-authored-by: Amit <ak47.amit@gmail.com>

--- shuffle/shuffle.py
+++ shuffle/shuffle.py
@@ -1,5 +1,12 @@
+"""Module to simulate roll for six-faced size."""
 import random
 
-class Shuffle(object):
+class Shuffle():
+    """Class to define a dice, incorrectly named Shuffle"""
     def roll_dice(self):
+        """Function to roll the dice."""
         return random.randint(1,6)
+
+    def roll_cheat(self):
+        """Function to return a specific roll."""
+        return 6
--- tests/test_shuffle.py
+++ tests/test_shuffle.py
@@ -1,9 +1,12 @@
+"""Test for shuffle class"""
 import unittest
 from shuffle.shuffle import Shuffle
 
 class ShuffleInPython(unittest.TestCase):
+    """Class to definte shuffle tests"""
 
     def test_range_should_be_between_one_and_six(self):
+        """Test shuffle test"""
         shuffler = Shuffle()
-        expectedResult = shuffler.roll_dice()
-        assert expectedResult > 0 and expectedResult <=6
+        expected_result = shuffler.roll_dice()
+        assert 0 < expected_result <=6 # > 0 and expected_result <=6
