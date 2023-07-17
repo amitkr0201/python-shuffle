94807f2da2eddf599093e924975d66ce9c8fd3f0
Amit Kumar <akumar105@sapient.com>
2023-07-17 14:45:10 +0100

    Update shuffle.py

--- shuffle/shuffle.py
+++ shuffle/shuffle.py
@@ -1,5 +1,7 @@
+"""Module to simulate roll for six-faced size."""
 import random
 
 class Shuffle(object):
+    """Class to define a dice, incorrectly named Shuffle"""
     def roll_dice(self):
         return random.randint(1,6)
