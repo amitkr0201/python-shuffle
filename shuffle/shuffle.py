da2f2706f5766436a612e1c85bc24e4c1dd2bf69
Amit <ak47.amit@gmail.com>
2023-07-17 14:58:03 +0100

    syntax error

--- shuffle/shuffle.py
+++ shuffle/shuffle.py
@@ -7,6 +7,6 @@ class Shuffle():
         """Function to roll the dice."""
         return random.randint(1,6)
 
-    def roll_cheat(self)
+    def roll_cheat(self):
         """Function to return a specific roll."""
         return 6
